c787465b0ac268a4fe3b8fdb671fad6bb7298082
Nasreen Fatima <129790265+nasreenrpx01@users.noreply.github.com>
2024-09-11 14:26:16 +0530

    Update app.py

--- app.py
+++ app.py
@@ -28,18 +28,6 @@ st.markdown("""
         background-repeat: no-repeat;
         z-index: 1;
     }
-    .main-container {
-        display: flex;
-        flex-direction: column;
-        background-color: rgba(255, 255, 255, 0.9);
-        border-radius: 10px;
-        box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
-        width: 300px; /* Adjust width as needed */
-        box-sizing: border-box;
-    }
-    .input-form-container {
-        margin-top: 50px; /* Space above the input form */
-    }
     .submit-btn, .back-btn {
         background-color: #4CAF50;
         color: white;
