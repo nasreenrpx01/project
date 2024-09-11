9e09de1ad419f25a9a4f624fe4ff29dea95a2403
Nasreen Fatima <129790265+nasreenrpx01@users.noreply.github.com>
2024-09-11 14:24:59 +0530

    Update app.py

--- app.py
+++ app.py
@@ -43,7 +43,6 @@ st.markdown("""
         background-color: rgba(255, 255, 255, 0.9);
         border-radius: 10px;
         box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
-        padding: 20px;
         width: 300px; /* Adjust width as needed */
         box-sizing: border-box;
     }
