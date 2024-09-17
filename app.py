438fc06e1a043ac2e7bea23c8fe1fc57a81ab672
Nasreen Fatima <129790265+nasreenrpx01@users.noreply.github.com>
2024-09-17 20:02:44 +0530

    Update app.py

--- app.py
+++ app.py
@@ -34,6 +34,10 @@ st.markdown("""
     .input-form-container {
         margin-top: 50px; /* Space above the input form */
     }
+     .main-container input {
+        color: white; /* Change input text color to white */
+        background-color: #333; /* Optional: change background to ensure contrast */
+    }
     .submit-btn, .back-btn {
         background-color: #4CAF50;
         color: white;
